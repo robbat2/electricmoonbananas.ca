15023fdc714296ebe8fdf8d36b60f449ce582878
Robin H. Johnson <robbat2@orbis-terrarum.net>
2021-06-23 07:21:58 +0000

    chore: capture index.php with mtime
    Signed-off-by: Robin H. Johnson <robbat2@orbis-terrarum.net>

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<?php include('moonphase-wrapper.inc.php'); ?>
+<head>
+<title>emb</title>
+</head>
+<body bgcolor="#000000" text="#ffffff">
+<center>
+<h1>Electric Moon Bananas</h1>
+
+Ride every saturday closest to the full moon!
+
+<h2>Last Ride</h2>
+<p>We rode the last saturday closest to the full moon.</p>
+
+<h2>Next Ride</h2>
+
+<p>We will ride the next saturday closest to the full moon.</p>
+
+<p>Meet at union and hawks, east vancouver, canaduh, between 10 and 11 pm. Continue onwards at 11pm.</p>
+
+<hr>
+<p>We have a mailing list (ride-electricmoonbananas-ca).  If you would
+like to be notified of future rides, send me an email
+(listmaster-electricmoonbananas-ca) and I'll put you on the list.</p>
+
+<p>IRC? <a href="irc://irc.libera.chat/%23emb">irc://irc.libera.chat/emb</a></p>
+
+<hr>
+
+<?php echo do_phase(time()); ?>
+</center>
+</body>
+</html>
